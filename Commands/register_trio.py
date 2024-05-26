92c4e4e44c1ba9a0ae5cd5c6ed25567840c72fb5
Catotron <cam29706@gmail.com>
2024-05-27 09:33:34 +1000

    Added reserve team system [v1.1.0-d16]

--- Commands/register_trio.py
+++ Commands/register_trio.py
@@ -1,5 +1,4 @@
 import nextcord
-import datetime
 import traceback
 from nextcord.ext import commands
 from Main import formatOutput, errorResponse, DB, getScrims, getScrim, getTeam, getMessages, splitMessage
@@ -26,8 +25,9 @@ class RegisterView(nextcord.ui.View):
                 if scrim['scrimConfiguration']['registrationChannel'] == interaction.channel.id:
                     scrim_name = scrim['scrimName']
                     break
-            
+
             team = getTeam(interaction.guild.id, scrim_name, interaction.message.embeds[0].title)
+            scrim = getScrim(interaction.guild.id, scrim_name)
             
             try:
                 if custom_id == "edit":
@@ -40,9 +40,49 @@ class RegisterView(nextcord.ui.View):
 
                 elif custom_id == "unregister":
                     if interaction.user.id == team["teamPlayer1"] or interaction.user.guild_permissions.administrator == True:
+                        msgindex = DB[str(interaction.guild.id)]["ScrimData"].find_one({"scrimName": scrim_name})["scrimConfiguration"]["registrationMessages"]
+
+                        # Reserve Message Editing
+                        if len(msgindex) < scrim['scrimConfiguration']['maxTeams'] - 1: # If there are less than max_teams (-1 to remove unregistered team)
+                            if scrim['scrimConfiguration']['reserveMessage'] != None: # and a reserve message exists, delete it
+                                reserve_message = await interaction.channel.fetch_message(scrim['scrimConfiguration']['reserveMessage'])
+                                await reserve_message.delete()
+
+                                DB[str(interaction.guild.id)]["ScrimData"].update_one({"scrimName": scrim_name}, {"scrimConfiguration.reserveMessage": None})
+                                DB[str(interaction.guild.id)]["ScrimData"].update_one({"scrimName": scrim_name}, {"$pull": {"scrimConfiguration.registrationMessages": reserve_message.id}})
+                        
+                        elif len(msgindex) >= scrim['scrimConfiguration']['maxTeams'] - 1: # If there are max_teams or more (-1 to remove unregistered team)
+                            # Then swap first reserve index[max_teams + 1] with index[max_teams] (then delete unregistered team index)
+                            # first_reserve refers to the team that was the first reserve | reserve_message refers to the message that was the reserve message
+                            reserve_message = await interaction.channel.fetch_message(scrim['scrimConfiguration']['IDs']['reserveMessage']) # Reserve Message
+                            first_reserve = await interaction.channel.fetch_message(msgindex[scrim['scrimConfiguration']['maxTeams']+ 1]) # First Reserve
+
+                            reserve_message_raw = splitMessage(getMessages(interaction.guild.id)['scrimReserve'], interaction.guild.id, scrim_name)
+                            reserve_message_embed = nextcord.Embed(title=reserve_message_raw[0], description=reserve_message_raw[1], color=White) # Build new reserve message
+
+                            first_reserve_embed = nextcord.Embed(title=first_reserve.embeds[0].title, description=first_reserve.embeds[0].description)
+                            if first_reserve.embeds[0].footer != None: first_reserve_embed.set_footer(text=first_reserve.embeds[0].footer.text) # Rebuild first reserve signup
+
+                            DB.Scrimotron.SavedMessages.update_one({"messageID": reserve_message.id}, {"$set": {"messageID": first_reserve.id}}) # Update saved messages
+
+                            message_data = DB.Scrimotron.SavedMessages.find_one({"messageID": first_reserve.id}) # Update view
+                            if message_data['viewType'] == "registration": view = RegisterView(interaction=DB.Scrimotron.SavedMessages.find_one({"messageID": first_reserve.id})["interactionID"])
+                            elif message_data['viewType'] == "checkin": view = CheckinView(interaction=DB.Scrimotron.SavedMessages.find_one({"messageID": first_reserve.id})["interactionID"])
+
+                            first_reserve_id = await reserve_message.edit(embed=first_reserve_embed, view=view) # Edit reserve message to first reserve
+                            reserve_message_id = await first_reserve.edit(embed=reserve_message_embed, view=None) # Edit first reserve to reserve message
+
+                            DB[str(interaction.guild.id)]["ScrimData"].update_one({"scrimName": scrim_name}, {"$set": {"scrimConfiguration.IDs.reserveMessage": reserve_message_id.id}})
+                            DB[str(interaction.guild.id)]["ScrimData"].update_one({"scrimName": scrim_name}, {"$set": {f"scrimTeams.{first_reserve.embeds[0].title}.messageID": first_reserve_id.id}})
+
+                        else: # Nothing happens (no reserves exist yet)
+                            pass
+
+                        DB[str(interaction.guild.id)]["ScrimData"].update_one({"scrimName": scrim_name}, {"$unset": {f"scrimTeams.{interaction.message.embeds[0].title}": ""}})
+                        DB[str(interaction.guild.id)]["ScrimData"].update_one({"scrimName": scrim_name}, {"$pull": {"scrimConfiguration.registrationMessages": team["messageID"]}})
+                       
                         message = await interaction.channel.fetch_message(team["messageID"])
                         await message.delete()
-                        DB[str(interaction.guild.id)]["ScrimData"].update_one({"scrimName": scrim_name}, {"$unset": {f"scrimTeams.{interaction.message.embeds[0].title}": ""}})
                         formatOutput(output=f"   {interaction.message.embeds[0].title} was unregistered!", status="Good", guildID=interaction.guild.id)
                     else:
                         embed = nextcord.Embed(title="Only the Team Captain and Admins can unregister teams!", color=Red)
@@ -90,9 +130,49 @@ class CheckinView(nextcord.ui.View):
 
                 elif custom_id == "unregister":
                     if interaction.user.id == team["teamPlayer1"] or interaction.user.guild_permissions.administrator == True:
+                        msgindex = DB[str(interaction.guild.id)]["ScrimData"].find_one({"scrimName": scrim_name})["scrimConfiguration"]["registrationMessages"]
+
+                        # Reserve Message Editing
+                        if len(msgindex) < scrim['scrimConfiguration']['maxTeams'] - 1: # If there are less than max_teams (-1 to remove unregistered team)
+                            if scrim['scrimConfiguration']['reserveMessage'] != None: # and a reserve message exists, delete it
+                                reserve_message = await interaction.channel.fetch_message(scrim['scrimConfiguration']['reserveMessage'])
+                                await reserve_message.delete()
+
+                                DB[str(interaction.guild.id)]["ScrimData"].update_one({"scrimName": scrim_name}, {"scrimConfiguration.reserveMessage": None})
+                                DB[str(interaction.guild.id)]["ScrimData"].update_one({"scrimName": scrim_name}, {"$pull": {"scrimConfiguration.registrationMessages": reserve_message.id}})
+                        
+                        elif len(msgindex) >= scrim['scrimConfiguration']['maxTeams'] - 1: # If there are max_teams or more (-1 to remove unregistered team)
+                            # Then swap first reserve index[max_teams + 1] with index[max_teams] (then delete unregistered team index)
+                            # first_reserve refers to the team that was the first reserve | reserve_message refers to the message that was the reserve message
+                            reserve_message = await interaction.channel.fetch_message(scrim['scrimConfiguration']['IDs']['reserveMessage']) # Reserve Message
+                            first_reserve = await interaction.channel.fetch_message(msgindex[scrim['scrimConfiguration']['maxTeams']+ 1]) # First Reserve
+
+                            reserve_message_raw = splitMessage(getMessages(interaction.guild.id)['scrimReserve'], interaction.guild.id, scrim_name)
+                            reserve_message_embed = nextcord.Embed(title=reserve_message_raw[0], description=reserve_message_raw[1], color=White) # Build new reserve message
+
+                            first_reserve_embed = nextcord.Embed(title=first_reserve.embeds[0].title, description=first_reserve.embeds[0].description)
+                            if first_reserve.embeds[0].footer != None: first_reserve_embed.set_footer(text=first_reserve.embeds[0].footer.text) # Rebuild first reserve signup
+
+                            DB.Scrimotron.SavedMessages.update_one({"messageID": reserve_message.id}, {"$set": {"messageID": first_reserve.id}}) # Update saved messages
+
+                            message_data = DB.Scrimotron.SavedMessages.find_one({"messageID": first_reserve.id}) # Update view
+                            if message_data['viewType'] == "registration": view = RegisterView(interaction=DB.Scrimotron.SavedMessages.find_one({"messageID": first_reserve.id})["interactionID"])
+                            elif message_data['viewType'] == "checkin": view = CheckinView(interaction=DB.Scrimotron.SavedMessages.find_one({"messageID": first_reserve.id})["interactionID"])
+
+                            first_reserve_id = await reserve_message.edit(embed=first_reserve_embed, view=view) # Edit reserve message to first reserve
+                            reserve_message_id = await first_reserve.edit(embed=reserve_message_embed, view=None) # Edit first reserve to reserve message
+
+                            DB[str(interaction.guild.id)]["ScrimData"].update_one({"scrimName": scrim_name}, {"$set": {"scrimConfiguration.IDs.reserveMessage": reserve_message_id.id}})
+                            DB[str(interaction.guild.id)]["ScrimData"].update_one({"scrimName": scrim_name}, {"$set": {f"scrimTeams.{first_reserve.embeds[0].title}.messageID": first_reserve_id.id}})
+
+                        else: # Nothing happens (no reserves exist yet)
+                            pass
+
+                        DB[str(interaction.guild.id)]["ScrimData"].update_one({"scrimName": scrim_name}, {"$unset": {f"scrimTeams.{interaction.message.embeds[0].title}": ""}})
+                        DB[str(interaction.guild.id)]["ScrimData"].update_one({"scrimName": scrim_name}, {"$pull": {"scrimConfiguration.registrationMessages": team["messageID"]}})
+                    
                         message = await interaction.channel.fetch_message(team["messageID"])
                         await message.delete()
-                        DB[str(interaction.guild.id)]["ScrimData"].update_one({"scrimName": scrim_name}, {"$unset": {f"scrimTeams.{interaction.message.embeds[0].title}": ""}})
                         formatOutput(output=f"   {interaction.message.embeds[0].title} was unregistered!", status="Good", guildID=interaction.guild.id)
                     else:
                         embed = nextcord.Embed(title="Only the Team Captain and Admins can unregister teams!", color=Red)
@@ -136,58 +216,56 @@ class RegisterDropdown(nextcord.ui.Select):
         scrim = getScrim(command['guildID'], self.values[0])
         if scrim['scrimConfiguration']['teamType'] == 'Trios': # Correct Team Type
             if self.team_data[0] not in scrim['scrimTeams'].keys(): # Check if team name is already in scrim
-                if len(scrim["scrimTeams"]) + 1 <= scrim["scrimConfiguration"]["maxTeams"]: # Check if team needs to be in reserve
-                    embed = nextcord.Embed(title=self.team_data[0], description=f"Player 1 - <@{self.IDs[1]}>\nPlayer 2 - <@{self.IDs[2]}>\nPlayer 3 - <@{self.IDs[3]}>\nSub 1 - {self.IDs[6]}\nSub 2 - {self.IDs[7]}")
-                    embed.set_thumbnail(url=self.team_data[1])
+                embed = nextcord.Embed(title=self.team_data[0], description=f"Player 1 - <@{self.IDs[1]}>\nPlayer 2 - <@{self.IDs[2]}>\nPlayer 3 - <@{self.IDs[3]}>\nSub 1 - {self.IDs[6]}\nSub 2 - {self.IDs[7]}")
+                embed.set_thumbnail(url=self.team_data[1])
 
-                    message = await self.interaction.guild.get_channel(scrim["scrimConfiguration"]["registrationChannel"]).send(embed=embed, view=RegisterView(self.interaction))
-                    DB.Scrimotron.SavedMessages.insert_one({"guildID": command['guildID'], "channelID": scrim["scrimConfiguration"]["registrationChannel"], "messageID": message.id, "interactionID": interaction.id, "viewType": "registration"})
+                message = await self.interaction.guild.get_channel(scrim["scrimConfiguration"]["registrationChannel"]).send(embed=embed, view=RegisterView(self.interaction))
+                DB.Scrimotron.SavedMessages.insert_one({"guildID": command['guildID'], "channelID": scrim["scrimConfiguration"]["registrationChannel"], "messageID": message.id, "interactionID": interaction.id, "viewType": "registration"})
+                DB[str(command['guildID'])]["ScrimData"].update_one({"scrimName": scrim['scrimName']}, {"$push": {"scrimConfiguration.registrationMessages": message.id}})
 
-                    DB[str(interaction.guild.id)]["ScrimData"].update_one(
-                        {"scrimName": scrim['scrimName']},
-                        {"$set": {f"scrimTeams.{self.team_data[0]}": {
-                            "teamType": "Trios",
-                            "teamLogo": self.team_data[1],
-                            "teamPlayer1": int(self.IDs[1]),
-                            "teamPlayer2": int(self.IDs[2]),
-                            "teamPlayer3": int(self.IDs[3]),
-                            "teamSub1": self.IDs[4],
-                            "teamSub2": self.IDs[5],
-                            "teamPois": {
-                                "map1": {
-                                    "map1POI": None,
-                                    "map1Secondary": None,
-                                    "map1Split": None,
-                                    "map1Vechicle": None},
-                                "map2": {
-                                    "map2POI": None,
-                                    "map2Secondary": None,
-                                    "map2Split": None,
-                                    "map2Vechicle": None}},
-                            "teamSetup": {
-                                "checkinMessageID": None,
-                                "poiMessageID": None,
-                                "roleID": None,
-                                "channelID": None},
-                            "teamStatus": {
-                                "checkin": False,
-                                "poiSelction": False},
-                            "messageID": int(message.id)
-                            },
-                        }})
-                    
-                    if len(scrim["scrimTeams"]) + 1 == scrim["scrimConfiguration"]["maxTeams"]: # Final Team, send reserve message
-                        message = splitMessage(getMessages(command['guildID'])['scrimReserve'], interaction.guild.id)
-                        message_split = message.split("\n")
-                        title = message_split[0]
-                        description = '\n'.join(message_split[1:])
+                DB[str(interaction.guild.id)]["ScrimData"].update_one(
+                    {"scrimName": scrim['scrimName']},
+                    {"$set": {f"scrimTeams.{self.team_data[0]}": {
+                        "teamType": "Trios",
+                        "teamLogo": self.team_data[1],
+                        "teamPlayer1": self.IDs[1],
+                        "teamPlayer2": self.IDs[2],
+                        "teamPlayer3": self.IDs[3],
+                        "teamSub1": self.IDs[4],
+                        "teamSub2": self.IDs[5],
+                        "teamPois": {
+                            "map1": {
+                                "map1POI": None,
+                                "map1Secondary": None,
+                                "map1Split": None,
+                                "map1Vechicle": None},
+                            "map2": {
+                                "map2POI": None,
+                                "map2Secondary": None,
+                                "map2Split": None,
+                                "map2Vechicle": None}},
+                        "teamSetup": {
+                            "checkinMessageID": None,
+                            "poiMessageID": None,
+                            "roleID": None,
+                            "channelID": None},
+                        "teamStatus": {
+                            "checkin": False,
+                            "poiSelction": False},
+                        "messageID": message.id
+                        },
+                    }})
+                                
+                if len(scrim["scrimTeams"]) + 1 == scrim["scrimConfiguration"]["maxTeams"]: # Final Team, send reserve message (+1 to include the new team)
+                    message = splitMessage(getMessages(command['guildID'])['scrimReserve'], command["guildID"], self.values[0])
 
-                        embed = nextcord.Embed(title=title, description=description, color=White)
-                        message = await interaction.guild.get_channel(scrim["scrimConfiguration"]["registrationChannel"]).send(embed=embed)
-                        DB[command['guildID']]['ScrimData'].update_one({"scrimName": scrim['scrimName']}, {"$set": {"scrimConfiguration.IDs.reserveMessage": message.id}})
+                    embed = nextcord.Embed(title=message[0], description=message[1], color=White)
+                    message = await interaction.guild.get_channel(scrim["scrimConfiguration"]["registrationChannel"]).send(embed=embed)
+                    DB[str(command['guildID'])]['ScrimData'].update_one({"scrimName": scrim['scrimName']}, {"$set": {"scrimConfiguration.IDs.reserveMessage": message.id}})
+                    DB[str(command['guildID'])]['ScrimData'].update_one({"scrimName": scrim['scrimName']}, {"$push": {"scrimConfiguration.registrationMessages": message.id}})
 
-                    embed = nextcord.Embed(title="Team Registered", description=f"**{self.team_data[0]}** has been registered for **{scrim['scrimName']}**", color=Green)
-                    await interaction.response.edit_message(embed=embed, view=None)
+                embed = nextcord.Embed(title="Team Registered", description=f"**{self.team_data[0]}** has been registered for **{scrim['scrimName']}**", color=Green)
+                await interaction.response.edit_message(embed=embed, view=None)
 
             else: # Team Name Already Exists
                 team = getTeam(command['guildID'], self.values[0], self.team_data[0])
